2c023c06e87c22de9175d13e1b14a8914487867b
Abdelhamid Bakhta <abdelhamid.bakhta@consensys.net>
2019-06-10 18:37:23 +0200

    introduce RBAC

--- contracts/Aiakos.sol
+++ contracts/Aiakos.sol
@@ -1,5 +1,6 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/access/Roles.sol";
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
@@ -10,9 +11,49 @@ import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract Aiakos is Ownable  {
 
   using SafeMath for uint256;
+  using Roles for Roles.Role;
+  
+  Roles.Role private maintainers;
 
-  constructor () public{
-      
+  uint private requiredNumberOfMaintainers;
+
+  constructor (uint _requiredNumberOfMaintainers) public{
+    Ownable._transferOwnership(msg.sender);
+    require(_requiredNumberOfMaintainers > 0);
+    requiredNumberOfMaintainers = _requiredNumberOfMaintainers;
+  }
+  
+  function addMaintainer(address _maintainerAddress)
+   public
+   onlyOwner
+   payable
+   nonEmptyAddress(_maintainerAddress)
+  {
+      maintainers.add(_maintainerAddress);
+  }
+  
+  function amIMaintainer() 
+   public
+   view
+   returns (bool)
+  {
+      return maintainers.has(msg.sender);
+  } 
+  
+  function isMaintainer(address _maintainerAddress) 
+   public
+   view
+   returns (bool)
+  {
+      return maintainers.has(_maintainerAddress);
+  }  
+  
+  function getRequiredNumberOfMaintainers() 
+   public
+   view
+   returns (uint)
+  {
+      return requiredNumberOfMaintainers;
   }
   
   /**
